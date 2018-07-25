8fac1ea194dee46f68dc6fb9f280a96cc6623f1a
Mayowa Falade <falademayowa240@gmail.com>
2018-07-25 17:17:10 +0100

    Add functions to compute country data, country name, and country code based on provided arguments

---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var countries = require('./data');
+console.log(countries.length, '....length ..')
+
+// Define mappings
+var namesMapping = {};
+var codesMapping = {};
+
+countries.forEach(function(country) {
+  namesMapping[country.name.toLowerCase()] = { alpha2Code: country.alpha2Code, alpha3Code: country.alpha3Code };
+  codesMapping[country.alpha2Code.toLowerCase()] = country.name;
+  codesMapping[country.alpha3Code.toLowerCase()] = country.name;
+});
+
+
+module.exports = {
+  getCountries,
+  getMapping,
+  getCountryName,
+  getCountryCode,
+  getAllCountryCodes,
+  getAllCountryNames
+};
+
+
+function normalizeString(str) {
+  if (typeof str !== 'string') {
+    return '';
+  }
+
+  return str.trim().toLowerCase();
+}
+
+function normalizeCountryCodeFormat(preferredFormat) {
+  var format = (normalizeString(preferredFormat) || 'alpha2') + 'Code';
+  if (format === 'alpha2Code' || format === 'alpha3Code') {
+    return format;
+  }
+
+  return 'alpha2Code';
+}
+
+function getCountryCode(countryName, preferredFormat) {
+  if (typeof countryName !== 'string') {
+    return null;
+  }
+
+  var normalizedCountryName = normalizeString(countryName);
+  var country = namesMapping[normalizedCountryName] || {};
+
+  var format = normalizeCountryCodeFormat(preferredFormat);
+  return country[format] || null;
+}
+
+function getCountryName(countryCode) {
+  var code = normalizeString(countryCode);
+  return codesMapping[code] || null;
+}
+
+function getAllCountryNames() {
+  return countries.map(function(country) {
+    return country['name'];
+  });
+}
+
+function getAllCountryCodes(preferredFormat) {
+  var format = normalizeCountryCodeFormat(preferredFormat);
+
+  return countries.map(function(country) {
+    return country[format];
+  });
+}
+
+function getMapping(preferredFormat) {
+  var format = normalizeCountryCodeFormat(preferredFormat);
+
+  return countries.map(function(country) {
+    return { [country[format]]: country['name'] }
+  });
+}
+
+function getCountries() {
+  return countries;
+}
